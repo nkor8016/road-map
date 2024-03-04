0318ec2874847555cc5effb4f9b12f73825f619e
Nayatha Yasiru <Nayantha.Koralegama@sysco.com>
2024-03-04 14:16:07 +0530

    reformat according to new data format.

--- create_map.py
+++ create_map.py
@@ -9,6 +9,9 @@ bars = pd.read_excel("./Project Road Map Data - SCM.xlsx", sheet_name="Bars")
 
 desired_format = "%Y-%m-%d"
 
+bars['Start_Date'] = pd.to_datetime(bars['Start_Date'])
+bars['End_Date'] = pd.to_datetime(bars['End_Date'])
+
 group_by_teams_data = bars.groupby("Team")
 
 for group_name, group_df in group_by_teams_data:
@@ -17,8 +20,8 @@ for group_name, group_df in group_by_teams_data:
     roadmap.set_title("Roadmap")
     roadmap.set_subtitle(f"SCM - {group_name}")
 
-    earliest_date = group_df["Start Date"].min()
-    latest_date = group_df["End Date"].max()
+    earliest_date = group_df["Start_Date"].min()
+    latest_date = group_df["End_Date"].max()
     difference = relativedelta(latest_date, earliest_date)
 
     roadmap.set_timeline(TimelineMode.MONTHLY, start=earliest_date.strftime(desired_format),
@@ -27,18 +30,18 @@ for group_name, group_df in group_by_teams_data:
     group = roadmap.add_group(str(group_name))
 
     for index, row in group_df.iterrows():
-        start_date = row['Start Date'].strftime(desired_format)
-        end_date = row['End Date'].strftime(desired_format)
+        start_date = row['Start_Date'].strftime(desired_format)
+        end_date = row['End_Date'].strftime(desired_format)
 
-        difference = relativedelta(row['End Date'], row['Start Date'])
+        difference = relativedelta(row['End_Date'], row['Start_Date'])
 
-        print(row['Title'], len(row['Title']), difference.months + difference.years * 12)
+        print(row['Release_Name'], len(row['Release_Name']), difference.months + difference.years * 12)
         task = None
-        if group_name in ["SWMS"] and difference.months + difference.years * 12 < 2 and len(row["Title"]) > 10:
+        if group_name in ["SWMS"] and difference.months + difference.years * 12 < 2 and len(row["Release_Name"]) > 10:
             task = group.add_task("", start_date, end_date)
         else:
             task = group.add_task(
-                row["Title"].replace("SWMS New UI - ", "")
+                row["Release_Name"].replace("SWMS New UI - ", "")
                 .replace("[New UI]", "").replace("[ARCHITECTURAL_ENHANCEMENT] ", "")
                 .replace("[Tech Debt]", "").replace("[ARCHITECTURAL_ENHANCEMENT]", "").replace("[SWMS]", "")
                 .replace("SWMS", "").replace("Version", "")
@@ -56,15 +59,15 @@ for group_name, group_df in group_by_teams_data:
     roadmap.save(f"./images/roadmap {group_name}.png")
 
 df_SWMS = group_by_teams_data.get_group("SWMS").drop("Team", axis=1)
-df_SWMS_grouped_by_initiatives = df_SWMS.groupby('Product/Initiatives')
+df_SWMS_grouped_by_initiatives = df_SWMS.groupby('Initiative')
 
 for initiative_name, initiative_df in df_SWMS_grouped_by_initiatives:
     roadmap = Roadmap(2000, 2000, colour_theme="BLUEMOUNTAIN")
     roadmap.set_title("Roadmap")
     roadmap.set_subtitle(f"SCM - {initiative_name}")
 
-    earliest_date = initiative_df["Start Date"].min()
-    latest_date = initiative_df["End Date"].max()
+    earliest_date = initiative_df["Start_Date"].min()
+    latest_date = initiative_df["End_Date"].max()
 
     difference = relativedelta(latest_date, earliest_date)
 
@@ -74,16 +77,16 @@ for initiative_name, initiative_df in df_SWMS_grouped_by_initiatives:
     group = roadmap.add_group(str(initiative_name))
 
     for index, row in initiative_df.iterrows():
-        start_date = row['Start Date'].strftime(desired_format)
-        end_date = row['End Date'].strftime(desired_format)
+        start_date = row['Start_Date'].strftime(desired_format)
+        end_date = row['End_Date'].strftime(desired_format)
 
-        difference = relativedelta(row['End Date'], row['Start Date'])
+        difference = relativedelta(row['End_Date'], row['Start_Date'])
 
         if initiative_name == "SWMS - UI Modernization (New UI)":
-            print(row['Title'], len(row['Title']), difference.months + difference.years * 12)
+            print(row['Release_Name'], len(row['Release_Name']), difference.months + difference.years * 12)
             if difference.months + difference.years * 12 > 2:
                 task = group.add_task(
-                    row["Title"].replace("SWMS New UI - ", "")
+                    row["Release_Name"].replace("SWMS New UI - ", "")
                     .replace("[New UI]", "").replace("[ARCHITECTURAL_ENHANCEMENT] ", "")
                     .replace("[Tech Debt]", "").replace("[ARCHITECTURAL_ENHANCEMENT]", "").replace("[SWMS]", "")
                     .replace("[QE][Automation] ", "").replace("Version ", ""),
@@ -91,7 +94,7 @@ for initiative_name, initiative_df in df_SWMS_grouped_by_initiatives:
             else:
                 task = group.add_task("", start_date, end_date)
         else:
-            task = group.add_task(row['Title'], start_date, end_date)
+            task = group.add_task(row['Release_Name'], start_date, end_date)
 
         task.add_milestone(start_date, start_date, text_alignment="left")
         task.add_milestone(end_date, end_date, text_alignment="right")
